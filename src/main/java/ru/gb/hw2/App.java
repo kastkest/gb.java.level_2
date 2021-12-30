a0c1af0b3a821dbb6ab3bc88b028e686ee723cdc
kastkest <endimion-xi@yandex.ru>
2021-12-30 15:04:12 +0300

    Add Array

--- src/main/java/ru/gb/hw2/App.java
+++ src/main/java/ru/gb/hw2/App.java
@@ -3,19 +3,22 @@ package ru.gb.hw2;
 public class App {
     public static void main(String[] args) {
 
-    fillArray(createArray());
+        fillArray(createArray());
 
     }
 
-    static int[] createArray (){
-        int[] a = new int[20];
+    static int[][] createArray() {
+        int[][] a = new int[4][4];
         return a;
     }
 
-    static int[] fillArray (int[] a) {
+    static int[][] fillArray(int[][] a) {
         for (int i = 0; i < a.length; i++) {
-            a[i] = (int) (Math.random()*9+1);
-            System.out.print(a[i] + " ");
+            for (int j = 0; j < a.length; j++) {
+                a[i][j] = (int) (Math.random() * 9 + 1);
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
         }
         System.out.println();
         return a;
