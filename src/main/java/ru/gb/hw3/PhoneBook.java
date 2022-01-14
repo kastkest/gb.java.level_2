52cb194531e0eaa8f98f0ad6209ae530e44e2d2f
kastkest <endimion-xi@yandex.ru>
2022-01-14 13:35:25 +0300

    Create HW3

---
+++ src/main/java/ru/gb/hw3/PhoneBook.java
@@ -0,0 +1,34 @@
+package ru.gb.hw3;
+
+import java.util.HashMap;
+
+public class PhoneBook extends HashMap {
+
+    public String add(String telNumber, String lastName) {
+        return (String) put(telNumber, lastName);
+    }
+
+    public void get(String lastName, PhoneBook phoneBook) {
+        System.out.println("По фамилии " + lastName + " в телефонной книге есть следующие номера:");
+        for (Object telNumber : phoneBook.keySet()) {
+            if (lastName == phoneBook.get(telNumber)) {
+                System.out.println(telNumber);
+            }
+        }
+    }
+}
+
+class go {
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("135", "Иванов");
+        phoneBook.add("495", "Иванов");
+        phoneBook.add("999", "Сидоров");
+        phoneBook.add("734", "Ефремова");
+        phoneBook.add("234", "Сидоров");
+
+        System.out.println(phoneBook);
+        phoneBook.get("Сидоров", phoneBook);
+    }
+}
+
